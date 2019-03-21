e70ccee755b974313512f0fef8055f24110d7350
Adam Simon <asimon@billtrust.local>
2019-03-21 16:17:52 -0400

    first commit

---
+++ dataload_api_cli.py
@@ -0,0 +1,70 @@
+'''
+Author: Adam Simon
+Date Created: 3/19/2019
+Description:
+CLI tool that can trigger actions on Dataload application by passing approiate
+query parameter 'action'
+'''
+import argparse
+import requests
+
+import dataload_api_core as dac
+
+ # ########################  DEFAULT URLs  ################################### #
+dl_base_url = 'http://dataload.vcmww.com/dataload/'
+dl_login_url = dl_base_url + 'DL_Login.jsp'
+dl_work_url = dl_base_url + 'DL_Work.jsp'
+
+# ########################## CREATE ARGS ##################################### #
+parser = argparse.ArgumentParser(description='CLI tool to automate clicking '  \
+                                             'of UI button to kickoff '        \
+                                             'dataload application processes')
+parser.add_argument('action' help='specify which action should be sent after'  \
+                                  ' with query param to URL')
+parser.add_argument('-u', '--user', help='valid username to pass to login page')
+parser.add_argument('-p', '--password', help='valid pwd to pass to login page')
+parser.add_argument('-l','--login_url',help='full url of login page')
+parser.add_argument('-t', '--target_url', help='url to target adter login')
+parser.add_argument('-o', '--output', help='folder path to send error file '   \
+                                            'created if login unsucessful')
+
+# ######################### CREATE LOG FILE ################################## #
+# TODO create logging functionality
+
+# ########################## MAIN FUNCTION ################################### #
+def run(args):
+    # post data applicaple to form on login page
+    post_data = {
+        'login': args.user,
+        'password': args.password
+    }
+    # set action passed in CLI to 'action' param of target url query
+    # newDriver requires submissionId of 0 to kick job off
+    if args.action == 'driver':
+        get_params = {
+            'action': 'newDriver',
+            'submissionId': '0'
+        }
+    # no submissionId needed for stage file job
+    else if args.action == 'stage':
+        get_params = {
+            'action': 'stageFiles'
+        }
+    # extending capability in case script needs to hit another login
+    if args.login_url:
+        login_url = args.login_url
+    else:
+        login_url = dl_login_url
+    # extending capabiilty to change target URl if needed
+    if args.target_url:
+        target_url = args.target_url
+    else:
+        target_url = dl_work_url
+
+    response = dac.scrape_data(login_url, target_url, post_data, get_params)
+    processed = dac.process_data(response)
+    return processed[0][3]
+
+if __name__ = '__main__':
+    args = parser.parse_args()
+    run(args)
---
+++ dataload_api_core.py
@@ -0,0 +1,93 @@
+'''
+app to login into dataload and attempt to scrape down all file processed
+for the day and any errors that might have occired
+auth: adam
+date: 11/21/18
+'''
+import datetime as dt
+import html
+import logging
+import os
+import pandas as pd
+import re
+import requests
+
+from bs4 import BeautifulSoup
+
+# constants
+COLS = ['id', 'media', 'name', 'state1', 'state2', 'file', 'receipt',
+        'format', 'load']
+
+# ######################## format logger #################################### #
+# grab date script is run for logging date
+
+def log_response():
+    log_date = dt.datetime.now()
+    # grab dir script exitst in, logging dir exists in that dir as well
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    log_dir = os.path.join(basedir,'log\\')
+    # create logger config object with date in filename, filename concats actual
+    # filename with basedir & logging dir
+    logging.basicConfig(filename=log_dir + '{}_dlscrape_log.txt'.format(log_date.strftime('%Y%m%d_%H%M%S')),
+                                 level=logging.INFO,
+                                 format='%(asctime)s %(message)s')
+
+# ######################### scrape data ##################################### #
+def scrape_data(login_url, target_url, post_data, get_params):
+    # opening session object in 'with' context to ensure proper closure once
+    # data is scraped and instantiated as bs4 object
+    with requests.Session() as sess:
+        # post to login form with approriate payload data created earlier
+        login_response = sess.post(login_url, data=post_data)
+        # check status code of returned response, 200 is expected if login was
+        # successful, otherwise print status code
+        if login_response.status_code != 200:
+            print('Uh oh, response code of {0} ' \
+                  'received back!?'.format(login_response.status_code))
+        else:
+            # if login was successful, scrape data off redirect page from login
+            homepage_response = sess.get(target_url, params=get_params)
+            return homepage_response
+
+# ####################### process raw data ################################### #
+def process_data(response):
+    # instatiate beautiful soup object to parse out data from html tree
+    targetpage_soup = BeautifulSoup(response.text, 'html.parser')
+    # declare list var to hold parsed raw data
+    delim = []
+    # regex pattern to pull text from within html tags, pattern only tracks
+    # alphanumeric and the following specials [&,.';-()], if any other specials
+    # are passed they text will not be captured
+    p2 = r">([\d\s\w&,.\';\-\(\)]+)<"
+    # find all 'tr' html tags, this equates to the html table on the target page
+    # that holds the data.  html.unescape() method translates any html chars
+    # into regular unicode
+    for i in targetpage_soup.find_all('tr'):
+        delim.append([html.unescape(i) for i in re.findall(p2, str(i))])
+    return delim
+
+# ##################### build result dataframe ############################### #
+def build_df(processed_data)
+    # grab only 1st 9 cols of the parsed data, the rest is not needed, also
+    # exculde last item, it's blank
+    delim2 = [i[:9] for i in processed_data[:-1]]
+    # grab index of column headers to exclude unecessary items, add 1 so col
+    # headers are not grabbed along with data
+    idx = int([delim2.index(i) for i in delim2 if i[0]  == 'Id'][0]) + 1
+    # drop data in dataframe for easy processing
+    df = pd.DataFrame.from_records(delim2[idx:], columns=COLS)
+    df['extension'] = df['file'].apply(lambda x :x.split('.')[-1])
+    return df
+
+# ################ check dataframe for fails and log ######################### #
+def alert_on_fails(df):
+    df_fails = df[df['state2'] == 'failed']
+    # TODO write the function to send the email inside of the gmail_pull_attach
+    # script, you need to turn that long script into function calls anyway
+    # then just import with the approriate information
+    if df_fails.empty:
+        logging.warning('No failed fails found')
+        return None
+    else:
+        logging.info('{}'.format(df_fails))
+        return df_fails
